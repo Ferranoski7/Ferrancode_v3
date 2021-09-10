a5a565797c0f10cd7ce5fc7fb698d438780b1a7c
Ferran Jimenez <ferransamc@gmail.com>
2021-09-10 13:10:28 +0200

    Added all Elements up to Rn, spline needs to either improve or be substituted

--- main.py
+++ main.py
@@ -6,24 +6,17 @@ from temp import TemporalFunction
 from symmetry_operations import SymOp
 import numpy as np
 
-
 llista = ['Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar']
-
 
 element = ['N']
 coord_a = [[0, 0, 0]]
-element=Molecule(element,coord_a,'N')
-temp_element=TemporalFunction(element)
+element = Molecule(element, coord_a, 'N')
+temp_element = TemporalFunction(element)
 
-wv_new=temp_element.gain_weights()
-
+wv_new = temp_element.gain_weights()
 
 rho, x = temp_element.elden.linear_valence_density()
 valence, _ = temp_element.linear_densityt(w=wv_new)
-
-
-
-
 
 plt.figure('Separation_Core_and_Valence_N')
 plt.plot(x, rho, label='Valence linear density')
@@ -33,16 +26,11 @@ plt.xlabel('r(A)')
 plt.ylabel('Radial distribution')
 plt.show()
 
-
 for atom in range(0):
-#for atom in range(len(llista)):
+    # for atom in range(len(llista)):
     element = [llista[atom]]
     coord_a = [[0, 0, 0]]
-    element=Molecule(element,coord_a,llista[atom])
-    temp_element=TemporalFunction(element)
+    element = Molecule(element, coord_a, llista[atom])
+    temp_element = TemporalFunction(element)
 
-    wv_new=temp_element.gain_weights()
-
-
-
-
+    wv_new = temp_element.gain_weights()
